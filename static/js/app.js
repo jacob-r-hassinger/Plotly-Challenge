580b68e7871a4106cb6f66135dd6529577ab6d60
Jake Hassinger <jacob.r.hassinger@gmail.com>
2020-04-29 18:48:56 -0400

    adding updates to app to produce gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -43,24 +43,47 @@ d3.json("data/samples.json").then((data) => {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     //dial chart
+    // find avg wash frequency for dataset
+    var washList = []
+    for (i = 0; i < dataFile["metadata"].length; i++) {
+        if (dataFile["metadata"][i]["wfreq"] != null) {
+            washList.push(dataFile["metadata"][i]["wfreq"]);
+        };   
+    };
+    console.log(washList);
 
-    var dialData = [
-        {domain: {x: [0,1], y:[0,1]},
-        value: 1,
-        title: "Belly Button Scrub Frequency",
-        mode: "gauge",
-        gauge: {
-            axis: { range: [0, 9]},
-            steps: [
-            {range: [0,1], color: "gray"},
-            {range: [2,3], color: "lightblue"},
-            {range: [4,5], color: "gray"},
-            {range: [6,7], color: "lightblue"},
-            {range: [8,9], color: "gray"}
-        ]}
-    }];
-    var dialLayout = {width: 600, height: 450, margin: {t:0, b:0}};
-    Plotly.newPlot("gauge", dialData, dialLayout);
+    var sumWashFreq =  0
+    
+    for (i=0; i < washList.length; i++) {
+        sumWashFreq += washList[i];
+    };
+
+    var avgWashFreq = sumWashFreq/washList.length;
+
+    console.log(avgWashFreq);
+
+    var dialdata = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: avgWashFreq,
+            title: "Average Belly Button Scrub Frequency",
+            type: "indicator",
+            mode: "gauge",
+            gauge: {
+                axis: { range: [0, 9]},
+                steps: [
+                    {range: [0,1], color: "gray"},
+                    {range: [2,3], color: "lightblue"},
+                    {range: [4,5], color: "gray"},
+                    {range: [6,7], color: "lightblue"},
+                    {range: [8,9], color: "gray"}
+                ]
+        }   
+        }
+    ];
+    
+    var dialLayout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', dialdata, dialLayout);
 
 
     //plotly new plot do this here
